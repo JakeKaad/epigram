5f33b1ef6f63adf63eec8a4a832049b89613fb21
Jake Kaad and Mike Goren <kaadalac@gmail.com>
2015-03-19 11:55:55 -0700

    add tag model specs, add tag user_id uniqueness validation

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ActiveRecord::Base
   belongs_to :tagged_user, class_name: "User", foreign_key: :user_id
   belongs_to :tagged_photo, class_name: "Photo", foreign_key: :photo_id
+
+  validates_uniqueness_of :user_id, scope: :photo_id
 end
